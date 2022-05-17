7cde2a38b1320cddaa54ce95b532f8cc85409131
Murilo <inayievmpl@gmail.com>
2022-05-17 16:01:36 -0300

    remove: interfaces.ts

--- src/interfaces.ts
+++
@@ -1,8 +0,0 @@
-import { SetStateAction } from "react";
-
-interface IPropsTimer {
-    timer: { workTime: number, breakTime: number, repeatTime: number };
-    setTimer: any;
-}
-
-export default IPropsTimer;
