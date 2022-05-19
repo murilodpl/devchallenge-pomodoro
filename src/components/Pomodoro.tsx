eabe09ee22abb5af165c0b2849d2449f7f0dda17
Murilo <inayievmpl@gmail.com>
2022-05-19 16:48:03 -0300

    style: pomodoro running page

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -5,9 +5,11 @@ export default function Pomodoro(props: any) {
     }
 
     return (
-        <>
-            <h1>Pomodoro Running</h1>
-            <button type="button" onClick={runTimer}>Início</button>
-        </>
+        <section className="pomodoro">
+            <nav>
+                <h1>Pomodoro</h1>
+                <button className="homeBtn" type="button" onClick={runTimer}>Início</button>
+            </nav>
+        </section>
     )
 }
